d7ceac80b87ca881be1da2d54e6174b04e22fb62
Ettore Leandro Tognoli <ettoreleandrotognoli@gmail.com>
2023-11-09 18:18:01 -0300

    PluginGroup and leafwing_input_manager

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,21 +1,77 @@
-use bevy::prelude::*;
+use bevy::{app::PluginGroupBuilder, prelude::*, sprite::MaterialMesh2dBundle};
+use bevy_rapier2d::prelude::*;
+use leafwing_input_manager::prelude::*;
+pub struct GamePlugins;
+
+#[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug, Reflect)]
+pub enum PlayerAction {}
+
+impl PluginGroup for GamePlugins {
+    fn build(self) -> PluginGroupBuilder {
+        let mut group = PluginGroupBuilder::start::<Self>();
+        group = group
+            .add(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+            .add(GamePlugin::default())
+            .add(InputManagerPlugin::<PlayerAction>::default());
+        #[cfg(debug_assertions)]
+        {
+            group = group.add(RapierDebugRenderPlugin::default());
+        }
+        group
+    }
+}
+#[derive(Component)]
+pub struct Player {}
+
+impl Default for Player {
+    fn default() -> Self {
+        Self {}
+    }
+}
 
 pub struct GamePlugin {}
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_camera_system);
+        app.add_systems(Startup, spawn_camera_system)
+            .add_systems(Startup, spawn_player_system);
     }
 }
 
 impl Default for GamePlugin {
     fn default() -> Self {
-        Self {
-            
-        }
+        Self {}
     }
+}
+
+pub fn spawn_world(mut commands: Commands) {
+    commands.insert_resource(RapierConfiguration {
+        gravity: Vec2::ZERO,
+        ..default()
+    });
 }
 
 pub fn spawn_camera_system(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+pub fn spawn_player_system(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let material = materials.add(ColorMaterial::from(Color::BLUE));
+    let circle = meshes.add(shape::Circle::new(32.).into());
+    commands
+        .spawn(Player::default())
+        .insert(Collider::ball(32.))
+        .insert(Velocity::linear(Vec2::new(0., 1.)))
+        .insert(Sleeping::disabled())
+        .insert(Ccd::enabled())
+        .insert(RigidBody::KinematicVelocityBased)
+        .insert(MaterialMesh2dBundle {
+            mesh: circle.into(),
+            material: material,
+            ..Default::default()
+        });
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use bevy_rapier2d::prelude::*;
 use gamejam_2023::game::*;
 
 fn main() {
-    App::new()
-        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
-        .add_plugins(RapierDebugRenderPlugin::default())
-        .add_plugins((DefaultPlugins, GamePlugin::default()))
-        .run();
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins).add_plugins(GamePlugins);
+    app.run();
 }
