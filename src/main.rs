5727208f3b3ff0c9820a370bea9be94ce15aa086
Ettore Leandro Tognoli <ettoreleandrotognoli@gmail.com>
2023-11-16 14:34:01 -0300

    wasm deploy fix

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
-use bevy::{prelude::*, window::WindowResolution};
+use bevy::prelude::*;
 
 use gamejam_2023::game::*;
 
+#[cfg(target_arch = "wasm32")]
+fn asset_plugin() -> AssetPlugin {
+    AssetPlugin {
+        mode: AssetMode::Unprocessed,
+        ..default()
+    }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn asset_plugin() -> AssetPlugin {
+    AssetPlugin {
+        mode: AssetMode::Processed,
+        ..default()
+    }
+}
+
 fn main() {
     let mut app = App::new();
-    app.add_plugins(
-        DefaultPlugins
-            .set(AssetPlugin {
-                mode: AssetMode::Processed,
-                ..default()
-            })
-            .set(WindowPlugin {
-                primary_window: Some(Window {
-                    resizable: false,
-                    resolution: (720., 1080.).into(),
-                    ..default()
-                }),
-                ..default()
-            }),
-    )
+    app.add_plugins(DefaultPlugins.set(asset_plugin()).set(WindowPlugin {
+        primary_window: Some(Window {
+            resizable: false,
+            resolution: (720., 1080.).into(),
+            ..default()
+        }),
+        ..default()
+    }))
     .add_plugins(GamePlugins);
     app.run();
 }
